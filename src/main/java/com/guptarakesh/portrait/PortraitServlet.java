b74ba1735a5127f78d8f03cd7b91a6a6bb3347b6
Rakesh Gupta <rakesh.gupta@salesforce.com>
2017-03-01 14:56:29 -0800

    initial checkin

---
+++ src/main/java/com/guptarakesh/portrait/PortraitServlet.java
@@ -0,0 +1,42 @@
+package com.guptarakesh.portrait;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class PortraitServlet extends HttpServlet {
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        
+        String ua = request.getHeader("User-Agent");
+        boolean mobile;
+        if (ua != null && ((ua.indexOf("Android") != -1) || (ua.indexOf("iPhone") != -1))) {
+            mobile = true;
+            request.setAttribute("doctype",
+                    "<!DOCTYPE html PUBLIC \"-//WAPFORUM//DTD XHTML Mobile 1.2//EN\" \"http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd\">");
+        } else {
+            mobile = false;
+            request.setAttribute("doctype",
+                    "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">");
+        }
+        
+        String nextView;
+        
+        if (mobile) {
+            nextView = "profile.jsp";
+        } else {
+            nextView = "profile.jsp";
+        }
+        
+        RequestDispatcher view = request.getRequestDispatcher(nextView);
+        view.forward(request, response);
+    }
+    
+}
